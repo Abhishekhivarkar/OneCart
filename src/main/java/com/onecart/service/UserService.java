44f9120158c118b9b1804ddd44bf4e519d19f33d
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-23 15:21:14 +0530

    feat: add user service layer

--- src/main/java/com/onecart/service/UserService.java
+++ src/main/java/com/onecart/service/UserService.java
@@ -1,5 +1,20 @@
 package com.onecart.service;
 
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+import com.onecart.entity.User;
+import java.util.List;
+@Service
 public class UserService {
-    
+    private List<User> userList = new ArrayList<>();
+
+    public List<User> fetchAllUsers(){
+        return userList;
+    }
+
+    public List<User> registerUser(User user){
+        userList.add(user);
+        return userList;
+    }
 }
