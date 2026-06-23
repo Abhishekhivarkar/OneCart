1050a6e4132e8773f7bf21dbdc044d1e29b617a3
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-06-23 11:55:33 +0530

    feat: add user entity with jpa annotations

---
+++ src/main/java/com/onecart/entity/User.java
@@ -0,0 +1,24 @@
+package com.onecart.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false, length = 100)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false, length = 100)
+    private String lastName;
+}
