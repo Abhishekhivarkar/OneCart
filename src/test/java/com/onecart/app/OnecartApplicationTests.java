f8f378e9ce5e2fb7fa9e65a6d32d3c74520d089a
Abhishek Hivarkar <abhihivarkar783@gmail.com>
2026-07-09 23:34:54 +0530

    Fix typo in SpringBootTest annotation

--- src/test/java/com/onecart/app/OnecartApplicationTests.java
+++ src/test/java/com/onecart/app/OnecartApplicationTests.java
@@ -3,7 +3,7 @@ package com.onecart.app;
 import org.junit.jupiter.api.Test; 
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTestb
 class OnecartApplicationTests { 
 
 	@Test
